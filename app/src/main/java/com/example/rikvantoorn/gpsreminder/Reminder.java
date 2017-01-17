803fed4542beb77ba4d2b9bdd363c120a3977980
RikvanToorn <rik.van.toorn@gmail.com>
2017-01-17 20:57:07 +0100

    google places API working and connection + adding files to database is working

---
+++ app/src/main/java/com/example/rikvantoorn/gpsreminder/Reminder.java
@@ -0,0 +1,55 @@
+package com.example.rikvantoorn.gpsreminder;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by Rik van Toorn on 16-1-2017.
+ */
+
+public class Reminder {
+    public String title;
+    public String location;
+    public String description;
+    public String date;
+
+    public int whenwarning;
+    public int distance;
+    public LatLng coordinates;
+
+
+    public Reminder(String title, String location, String description, String date, int distance,  LatLng coordinates, int whenwarning) {
+        this.title = title;
+        this.location = location;
+        this.description = description;
+        this.date = date;
+
+        this.distance = distance;
+        this.whenwarning = whenwarning;
+
+        this.coordinates = coordinates;
+    }
+
+    public String gettitle() {
+        return title;
+    }
+
+    public String getlocation() {
+        return location;
+    }
+
+    public String getdescription() {
+        return description;
+    }
+
+    public String getdate() {
+        return date;
+    }
+
+    public Integer getdistance() { return distance; }
+
+    public Integer getwhenwarning() {
+        return whenwarning;
+    }
+
+    public LatLng getcoordinates() { return coordinates; }
+}
--- app/src/main/java/com/example/rikvantoorn/gpsreminder/ReminderAddActivity.java
+++ app/src/main/java/com/example/rikvantoorn/gpsreminder/ReminderAddActivity.java
@@ -1,16 +1,63 @@
 package com.example.rikvantoorn.gpsreminder;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class ReminderAddActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private FirebaseAuth firebaseAuth;
+
+    private DatabaseReference databaseReference;
+
+    private Reminder reminder;
+
+    private LatLng latlng;
+    private String location;
+    private String date;
+
+    private int whenwarning;
+    private int distance;
 
     private Button buttonToReminderList;
     private Button buttonToMap;
     private Button buttonToReminder;
+    private Button buttonAddReminderAdd;
+
+    private TextView textViewDistance;
+
+    private EditText editTextReminderTitle;
+    private EditText editTextReminderDescription;
+
+    private CheckBox checkBoxEnteringLocation;
+    private CheckBox checkBoxLeavingLocation;
+
+    private SeekBar seekBarDistance;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +67,116 @@ public class ReminderAddActivity extends AppCompatActivity implements View.OnCli
         buttonToReminderList = (Button) findViewById(R.id.buttonToReminderList);
         buttonToMap = (Button) findViewById(R.id.buttonToMap);
         buttonToReminder = (Button) findViewById(R.id.buttonToReminder);
+        buttonAddReminderAdd = (Button) findViewById(R.id.buttonAddReminderAdd);
+
+        editTextReminderTitle = (EditText) findViewById(R.id.editTextReminderTitle);
+        editTextReminderDescription = (EditText) findViewById(R.id.editTextReminderDescription);
 
         buttonToReminderList.setOnClickListener(this);
         buttonToMap.setOnClickListener(this);
         buttonToReminder.setOnClickListener(this);
+        buttonAddReminderAdd.setOnClickListener(this);
+
+        textViewDistance = (TextView) findViewById(R.id.textViewDistance);
+        seekBarDistance = (SeekBar) findViewById(R.id.seekBarDistance);
+
+        checkBoxEnteringLocation = (CheckBox) findViewById(R.id.checkBoxEnteringLocation);
+        checkBoxLeavingLocation = (CheckBox) findViewById(R.id.checkBoxLeavinglocation);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user == null) {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        databaseReference = FirebaseDatabase.getInstance().getReference(user.getUid());
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                //.enableAutoManage(this, this)
+                .build();
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        ((EditText)autocompleteFragment.getView().findViewById(R.id.place_autocomplete_search_input)).setHint("Location");
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                latlng = place.getLatLng();
+                location = "" + place.getName();
+            }
+
+            @Override
+            public void onError(Status status) {
+                //Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+        seekBarDistance.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                textViewDistance.setText(String.valueOf(progress) + "M");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
+
+    private void addReminder() {
+        String title = editTextReminderTitle.getText().toString().trim();
+        String description = editTextReminderDescription.getText().toString().trim();
+        distance = seekBarDistance.getProgress();
+        date = DateFormat.getDateTimeInstance().format(new Date());
+
+        if(TextUtils.isEmpty(location)){
+            Toast.makeText(this, "Please enter an location", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(title)){
+            Toast.makeText(this, "Please enter an title", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (distance == 0) {
+            Toast.makeText(this, "Please enter a distance higher then 0M", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (( checkBoxEnteringLocation).isChecked()) {
+            whenwarning = whenwarning + 1;
+        }
+
+        if (( checkBoxLeavingLocation).isChecked()) {
+            whenwarning = whenwarning + 2;
+        }
+
+        if (whenwarning == 0) {
+            Toast.makeText(this, "Please check one or both options", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        reminder = new Reminder(title, location, description, date, distance, latlng, whenwarning );
+
+        DatabaseReference childRef = databaseReference.child("Reminders").child(title);
+        childRef.setValue(reminder);
+    }
+
+
 
     @Override
     public void onClick(View view) {
@@ -36,6 +188,9 @@ public class ReminderAddActivity extends AppCompatActivity implements View.OnCli
         }
         if(view == buttonToReminder) {
             startActivity(new Intent(this, ReminderActivity.class));
+        }
+        if(view == buttonAddReminderAdd) {
+            addReminder();
         }
     }
 }
